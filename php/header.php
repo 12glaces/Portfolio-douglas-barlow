ca7a8d65e46e31bb6b36e07fbdd0dfb80e5e79c8
BigBOii <d.barlow@laposte.net>
2021-11-03 15:52:07 +0100

    :lipstick::art: header for message login contact page

--- php/header.php
+++ php/header.php
@@ -16,3 +16,34 @@
     <title>Portfolio de Douglas Barlow</title>
     <link rel="shortcut icon" type="image/x-icon" href="../img/logo2.png" />
 </head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+          <a href="https://douglas-barlow.com/" class="logo">
+              <img src="../img/logo2.png" alt="logo">
+          </a>          
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 left menu-right">
+            <li class="nav-item">
+              <a class="nav-link bold" aria-current="page" href=<?php echo "https://".$_SERVER['HTTP_HOST']."/#presentation";?>>Présentation</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link bold" aria-current="page" href=<?php echo "https://".$_SERVER['HTTP_HOST']."/#competences";?>>Compétences</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link bold" aria-current="page" href=<?php echo "https://".$_SERVER['HTTP_HOST']."/#realisations";?>>Réalisations</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link bold" aria-current="page" href="contact.php">Contact</a>
+            </li>                                     
+          </ul>          
+        </div>
+      </div>
+    </nav>
+    <center>
+<?php
+session_start();
+?>
