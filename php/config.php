e9ee86b8fbd0e4031be3ca7db9d2120ee0493efb
BigBOii <d.barlow@laposte.net>
2021-11-03 10:37:44 +0100

    added config file

---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('display_errors', 1);
+$dsn = 'mysql:host=sql302.epizy.com;port=3306;dbname=epiz_29304690_douglasting';
+$user = 'epiz_29304690';
+$password = 'iZhnbVrYnn';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+?>
